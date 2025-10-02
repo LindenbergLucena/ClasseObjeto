26f5b85fcbefbb362fb5c3bb49cf35d34fa17451
LindenbergLucena <begapb@gmail.com>
2025-10-02 14:25:59 -0300

    Calculadora

---
+++ calculadora.py
@@ -0,0 +1,15 @@
+x = int(input('Digite o valor de x: '))
+y = int(input("Digite o valor de y: "))
+# Operações
+soma = x + y 
+subtracao = x - y 
+multiplicacao = x * y 
+divisao = x / y
+exponenciacao = x ** y 
+# Resultados
+print()
+print("A soma é negativa: ",soma)
+print("A subtração é: ",subtracao)
+print("A multiplicação é: ",multiplicacao)
+print("A divisao é: ",divisao)
+print("A exponenciação é: ",exponenciacao)
