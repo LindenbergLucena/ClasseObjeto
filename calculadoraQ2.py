b9a1cb08851abb0df92f14439fe6443a247e4013
LindenbergLucena <begapb@gmail.com>
2025-10-06 22:42:06 -0300

    Calculadora + - * /

--- calculadoraQ2.py
+++ calculadoraQ2.py
@@ -7,12 +7,24 @@ class Calculadora:
 
     def subtrair(self, a, b):
         return a - b
+    
+    def multiplicar(self, a, b):
+        return a * b 
+    
+    def dividir(self, a, b):
+        return a / b 
 # Criação do Objeto
 
 calc = Calculadora()
 # Chamar os métodos e impressão dos resultados
-resultado_soma = calc.somar(15, 7)
-resultado_subtracao = calc.subtrair(25, 8)
+a = float(input("Digite o valor de a: "))
+b = float(input("Digite o valor de b: "))
+resultado_soma = calc.somar(a, b)
+resultado_subtracao = calc.subtrair(a, b)
+resultado_multiplicar = calc.multiplicar(a, b)
+resultado_dividir = calc.dividir(a, b)
 
-print(f"A soma 15 + 7 é: {resultado_soma}")
-print(f"A subtração 25 - 8 é: {resultado_subtracao}")
+print(f"\nA soma {a} + {b} é: {resultado_soma:.2f}")
+print(f"\nA subtração {a} - {b} é: {resultado_subtracao:.2f}")
+print(f"\nA multiplicação {a} * {b} é: {resultado_multiplicar:.2f}")
+print(f"\nA divisão {a} / {b} é: {resultado_dividir:.2f}")
